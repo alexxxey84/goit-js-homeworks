07b772e42dc0e5c6013140b5ae096c898a591097
Alex Barna <barna8400@gmail.com>
2019-05-09 23:03:24 +0300

    hw01

---
+++ 00test/src/test.js
@@ -0,0 +1,128 @@
+'use strict';
+
+// const number = 35;
+
+// const name = 'Dexter';
+
+// const isActive = true;
+
+// const type = typeof isActive;
+
+// alert(777);
+
+// console.log(window);
+
+//=================================================
+
+// const isGoing = confirm('do you want to learn');
+
+// console.log(isGoing);
+
+//=================================================
+
+// const input = prompt('Write your name');
+
+// console.log(typeof input);
+//=================================================
+
+// const min = 10;
+// const max = 50;
+// const number = 60;
+
+// const isInRange = number > 10 && number < 50;
+
+// console.log(isInRange);
+
+//=================================================
+
+// console.log(1 === 1);
+
+//=================================================
+
+// let input = prompt('Угадай число от 1 до 10');
+
+// input = Number(input);
+
+// const isCorrect = input >= 1 && input <= 10;
+
+// console.log(isCorrect);
+
+//================================================
+
+// const boxWidth = '24px';
+
+// const width = Number.parseInt(boxWidth);
+
+// console.log(width);
+
+//================================================
+
+// let input = prompt('Угадай число от 1 до 8');
+
+// console.log('input перед преобразованием', input);
+
+// input = Number(input);
+
+// const isNan = Number.isNaN(input);
+
+// console.log('input после преобразованием', input);
+
+// console.log('isNan?', isNan);
+
+//================================================
+
+// console.log((0.2 * 10 + 0.1 * 10) / 10);
+
+// const number = Math.floor(1.7);
+// const number = Math.ceil(1.7);
+// const number = Math.round(2.7);
+// console.log(number);
+
+//================================================ Строки
+// console.log(5 + '7');
+// console.log(5 + 5 + '7');
+// console.log('1' + 5 + 5);
+// // '1' + 5 + 5 = '15' + 5 = '155'
+
+//====================================================
+
+// const productTitle = 'Samsung smartphone';
+
+// const searchString = 'Rams';
+
+// const titleInLC = productTitle.toLowerCase();
+
+// const searchStringInLC = searchString.toLowerCase();
+
+// const passed = titleInLC.includes(searchStringInLC);
+
+// console.log(passed);
+
+//=======================================================
+
+// const firstName = 'Alex';
+// const secondName = 'Bar';
+// const fullName = `${firstName} ${secondName}`;
+// console.log(fullName);
+
+// const name = 'Alex';
+
+// const age = 2;
+
+// const mood = 'happy';
+
+const message = `My name is ${name}, I'm ${age} years old and ${mood}.`;
+
+// console.log(message);
+
+//=========================================================
+
+// const date = 1;
+
+// const month = 8;
+
+// const year = 1984;
+
+// console.log(`${date}\\${month}\\${year}`);
+
+//===========================================
---
+++ mod01/src/hw01.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// ЗАДАЧА 1
+
+// const ADMIN_PASSWORD = 'm4ng0h4ckz';
+
+// const input = prompt('Введите пароль Админиcтратора');
+
+// let message;
+
+// if (input === ADMIN_PASSWORD) {
+//   message = 'Добро пожаловать!';
+// } else if (input === null) {
+//   message = 'Отменено пользователем!';
+// } else {
+//   message = 'Доступ запрещен, неверный пароль!';
+// }
+
+// alert(message);
+
+// ЗАДАЧА 2
+
+// const credits = 23580;
+
+// const pricePerDroid = 3000;
+
+// let input = prompt('Какое количество дроидов Вы желаете купить?');
+
+// let totalPrice;
+
+// let message;
+
+// if (input === null) {
+//   message = 'Отменено пользователем!';
+// } else {
+//   totalPrice = input * pricePerDroid;
+// }
+
+// if (totalPrice > credits) {
+//   message = 'Недостаточно средств на счету';
+// } else {
+//   let balance = credits - totalPrice;
+//   message = `Вы купили ${input} дроидов, на счету осталось ${balance} кредитов.`;
+// }
+
+// console.log(message);
+
+// ЗАДАЧА 3
+
