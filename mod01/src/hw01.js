412f7a4d3e29cf9aa24119af01f1ce252b58f90d
Alex Barna <barna8400@gmail.com>
2019-05-10 22:45:09 +0300

    HW01 js file

--- mod01/src/hw01.js
+++ mod01/src/hw01.js
@@ -44,3 +44,37 @@
 
 // ЗАДАЧА 3
 
+const input = prompt('Укажите страну доставки');
+
+let deliveryPrice;
+
+const country = input.toLowerCase();
+
+let message;
+
+switch (country) {
+  case 'китай':
+    deliveryPrice = 100;
+    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
+    break;
+  case 'южная америка':
+    deliveryPrice = 250;
+    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
+    break;
+  case 'австралия':
+    deliveryPrice = 170;
+    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
+    break;
+  case 'индия':
+    deliveryPrice = 80;
+    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
+    break;
+  case 'ямайка':
+    deliveryPrice = 120;
+    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
+    break;
+  default:
+    message = 'В вашей стране доставка не доступна';
+}
+
+console.log(message);
